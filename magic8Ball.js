93b73f7213f908d90d090a8b659b2d69a62618ec
Tracy DeWitt <Justdewitt8485@gmail.com>
2020-03-30 11:16:13 -0500

    Added Content to Array

--- magic8Ball.js
+++ magic8Ball.js
@@ -2,7 +2,7 @@
 let div = document.getElementById("answer");
 let buttonR = document.getElementById("click");
 let inputQ = document.getElementById("question");
-const answerArr = ["Your eyes can deceive you; Don't trust them.", "Only the Blurrg know.", "That's what she said.", "Darmok and Jalad at Tanagra.", "SoHvaD pagh vIjatlh, Human!", "I know just what to say, But in a much more real sense I have know idea what to say.", "You can't handle the truth.", "01111001 01100101 01110011", "01101110 01101111.", "Go ask the Southern Oracle.", "I am sure Morla'The Ancient'One knows that one, but they may not care."];
+const answerArr = ["Your eyes can deceive you; Don't trust them.", "Only the Blurrg know.", "That's what she said.", "Darmok and Jalad at Tanagra.", "SoHvaD pagh vIjatlh, Human!", "I know just what to say, But in a much more real sense I have know idea what to say.", "You can't handle the truth.", "01111001 01100101 01110011", "01101110 01101111.", "Go ask the Southern Oracle.", "I am sure Morla 'The Ancient One' knows that one, but they may not care."];
 console.log(answerArr);
 
 let ranAnswer = answerArr[Math.floor((Math.random() * answerArr.length))];
